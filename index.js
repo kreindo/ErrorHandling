8019ff97f25a7b73937903fcf87d817e7b753278
ahmadsan <38377576+kreindo@users.noreply.github.com>
2022-07-04 06:49:16 +0000

    added more scenarios

--- index.js
+++ index.js
@@ -70,3 +70,47 @@ try {
     console.log(error.name);
     console.log(error.message);
 }
+
+console.log(`---------------------------------`)
+
+let json5 = '{ "name": "Yoda", "age": 20 }';
+ 
+try {
+    let user = JSON.parse(json5);
+ 
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+ 
+    errorCode;
+ 
+    console.log(user.name); // Yoda
+    console.log(user.age);  // 20
+} catch (error) {
+    console.log(`JSON Error: ${error.message}`);
+}
+
+console.log(`-=>`)
+
+let json6 = '{ "name": "Yoda", "age": 20 }';
+
+try {
+    let user = JSON.parse(json6);
+ 
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+ 
+    errorCode;
+ 
+    console.log(user.name); // Yoda
+    console.log(user.age);  // 20
+} catch (error) {
+    if (error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if (error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
+}
